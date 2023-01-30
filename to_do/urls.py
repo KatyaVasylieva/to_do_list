4febc113a920ad635482e1325affb507b92263b8
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-30 13:58:39 +0200

    Add path for task_create view

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import Index
-
+from .views import Index, TaskCreateView
 
 urlpatterns = [
     path("", Index.as_view(), name="index"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
 ]
 
 app_name = "to-do"
