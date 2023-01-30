24d4e78733c32971fb40f6fea5f0800008fde12e
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-30 14:06:41 +0200

    Create paths fot car_update and car_delete views

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import Index, TaskCreateView
+from .views import (
+    Index,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+)
 
 urlpatterns = [
     path("", Index.as_view(), name="index"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 app_name = "to-do"
