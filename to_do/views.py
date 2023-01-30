e37c7994f49a1b71c10075f048576ca52157c075
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-30 13:58:59 +0200

    Create task_create view

--- to_do/views.py
+++ to_do/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.views import generic
 
 from to_do.models import Task
@@ -6,3 +7,12 @@ from to_do.models import Task
 class Index(generic.ListView):
     queryset = Task.objects.order_by("-is_done", "-created_time")
     template_name = "to_do/index.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    template_name = "to_do/task_create_form.html"
+
+    # def get_success_url(self):
+    #     return reverse("to-do:index", kwargs={"pk": self.object.pk})
