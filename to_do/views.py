54b65fd63fd564f82253679f993552c385ff728b
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-30 13:34:57 +0200

    Correct queryset ordering in index view

--- to_do/views.py
+++ to_do/views.py
@@ -4,5 +4,5 @@ from to_do.models import Task
 
 
 class Index(generic.ListView):
-    queryset = Task.objects.order_by(["-is_done", "-created_time"])
+    queryset = Task.objects.order_by("-is_done", "-created_time")
     template_name = "to_do/index.html"
