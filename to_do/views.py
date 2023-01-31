39f07db00475d1be394a050e933c3edea50b2a93
Katya Vasylieva <katevvasylyeva@gmail.com>
2023-01-31 16:15:59 +0200

    Correct queryset ordering in index view

--- to_do/views.py
+++ to_do/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponseRedirect
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.views import generic
 
 from to_do.models import Task, Tag
 
 
 class Index(generic.ListView):
-    queryset = Task.objects.order_by("-is_done", "-created_time")
+    queryset = Task.objects.order_by("is_done", "-created_time")
     template_name = "to_do/index.html"
 
 
